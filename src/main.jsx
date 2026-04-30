15dbeb0f54445fc193e777304a92a0346f46cfc4
annamoyosore <annamoyosore@gmail.com>
2026-04-30 04:12:46 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import WhotGame from "./WhotGame";
+import BottleGame from "./BottleGame";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <WhotGame />
-  </React.StrictMode>
-);
+function App() {
+  const [currentGame, setCurrentGame] = useState("whot");
+
+  return (
+    <React.StrictMode>
+      <div style={{ textAlign: "center" }}>
+        
+        {/* 🔥 Game Selector */}
+        <div style={{ marginBottom: "20px" }}>
+          <button onClick={() => setCurrentGame("whot")}>
+            Play Whot
+          </button>
+
+          <button
+            onClick={() => setCurrentGame("bottle")}
+            style={{ marginLeft: "10px" }}
+          >
+            Play Bottle Flip
+          </button>
+        </div>
+
+        {/* 🎮 Game Renderer */}
+        {currentGame === "whot" && <WhotGame />}
+        {currentGame === "bottle" && <BottleGame />}
+      </div>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
